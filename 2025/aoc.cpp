b89bee9c35a8c021575d5e8b1ee72f5aaa1895d3
Ben Kyd <benk@Bens-MacBook-Air.local>
2025-10-30 09:11:56 +0000

    2025 framework

---
+++ 2025/aoc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Advent of Code 2025 runner" << std::endl;
+    std::cout << "©Ben Kyd 2025, All Rights Reserved" << std::endl;
+
+    std::cout << "\n\n Usage: -d [day] (omit for all)" << std::endl;
+
+    // Parse command line
+
+    // Find days
+
+    // Run days
+}
+
